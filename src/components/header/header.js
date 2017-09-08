dd390ffb16fcdd1277004e9c8746cbfca65ab5a6
David Wieczorek <blackratio@gmail.com>
2017-09-09 00:39:21 +0200

    toto

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './w.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo home">
+          <NavLink exact to="/" activeClassName="active">
+            <img className="welcome-logo" src={logo} alt="Logo" />
+          </NavLink>
+        </div>
+        <nav>
+          <ul className="nav nav-vertical">
+            <li>
+              <NavLink to="/graphs" activeClassName="active">
+                graphs
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, HashRouter } from 'react-router-dom';
+
+import App from './app';
+import Home from './views/home';
+import Graphs from './views/graphs';
+
+const Routes = () =>
+  <HashRouter>
+    <App>
+      <Route exact path="/" component={Home} />
+      <Route path="/graphs" component={Graphs} />
+    </App>
+  </HashRouter>;
+
+export default Routes;
