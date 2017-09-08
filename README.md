1c2a21b65f2c85a3ea3beba16fc9c7e53b5534d5
David Wieczorek <blackratio@gmail.com>
2017-09-08 23:38:06 +0200

    change infos

---
+++ README.md
@@ -0,0 +1,6 @@
+Jokers D4 WebApp
+
+## Installation
+Install package with yarn `yarn` or `npm install`</br>
+Just run `npm start` and open [http://localhost:3000](http://localhost:3000) to view it in the browser.</br>
+Have fun :-)
---
+++ src/views/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Homelogo from '../../components/home_logo/home_logo';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Homelogo />
+      </div>
+    );
+  }
+}
+
+export default Home;
