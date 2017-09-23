30dc91458c9cd5723dab64051a18c47357756db5
David Wieczorek <blackratio@gmail.com>
2017-09-23 14:24:55 +0200

    adding library

--- src/app.js
+++ src/app.js
@@ -12,32 +12,4 @@ class App extends Component {
   }
 }
 
-class Toto {
-  constructor(params, par) {
-    this.titi = params;
-    this.tutu = par;
-  }
-  getTiti() {
-    const tern = this.titi;
-    function tata() {
-      return tern;
-    }
-    return tata();
-  }
-  getTutu(tt) {
-    return this.tutu + tt;
-  }
-}
-
-const book = new Toto('alfred', 'rené');
-console.log(book.getTutu(2));
-
-const funArray = [['🍾', 'Champagne'], ['🍭', 'Lollipop'], ['🎊', 'Confetti']];
-
-var ee = funArray.filter(function(n) {
-  return (n = '🍾');
-});
-
-console.log(ee);
-
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route, HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from '../reducers';
 
 import App from '../app';
 
@@ -8,14 +11,18 @@ import Team from './route_team';
 
 const routes = [Home, Team];
 
+const store = createStore(reducer);
+
 const routeComponents = routes.map(({ path, component }, key) => (
   <Route exact path={path} component={component} key={key} />
 ));
 
 const Routes = () => (
-  <HashRouter>
-    <App>{routeComponents}</App>
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App>{routeComponents}</App>
+    </HashRouter>
+  </Provider>
 );
 
 export default Routes;
